9c3aa090dd4a9c8ff533a91fd1680435907dd450
NGUYEN VAN DAT <vdat319@gmail.com>
2025-10-20 18:15:22 +0700

    ảnh đại diện admin

--- FE_Laptop-master/src/components/header/AdminHeader.jsx
+++ FE_Laptop-master/src/components/header/AdminHeader.jsx
@@ -1,29 +1,33 @@
-import React, { useState } from 'react';
-import './adminHeader.scss';
-import { FiMenu } from 'react-icons/fi'; // Import menu icon from react-icons
+import React, { useState } from "react";
+import "./adminHeader.scss";
+import { FiMenu } from "react-icons/fi"; // Import menu icon from react-icons
 
 const AdminHeader = ({ onToggleSidebar, isSidebarOpen }) => {
-
   const [userData, setUserData] = useState(() => {
-    const savedUser = localStorage.getItem('USER_LOGIN');
+    const savedUser = localStorage.getItem("USER_LOGIN");
     return savedUser ? JSON.parse(savedUser) : null;
   });
   return (
     <header className="header">
       <div className="header-left">
-        <button 
-          className="sidebar-toggle" 
+        <button
+          className="sidebar-toggle"
           onClick={onToggleSidebar}
           aria-label={isSidebarOpen ? "Close sidebar" : "Open sidebar"}
         >
           <FiMenu size={20} />
         </button>
       </div>
-      
+
       <div className="header-right">
         <div className="user-profile">
           <div className="user-avatar">
-            <img src={"https://i.pravatar.cc/150?img=3" } alt={userData.name} />
+            <img
+              src={
+                "https://png.pngtree.com/png-vector/20220817/ourmid/pngtree-man-avatar-with-circle-frame-vector-ilustration-png-image_6110328.png"
+              }
+              alt={userData.name}
+            />
           </div>
           <span className="user-name">{userData.fullName}</span>
         </div>
